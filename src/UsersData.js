0430068a3be93e01f16e0b132b55297e5f3e2499
Andres <afga199714@gmail.com>
2019-09-08 21:38:28 -0500

    correcion

--- src/UsersData.js
+++ src/UsersData.js
@@ -1,9 +1,9 @@
 const users = [
-    {name: 'Andres Giraldo', address: '5biilg2kd': users_voters: [] },
-    {name: 'Felipe Prueba', address: 'l21c41i72': users_voters: [] },
-    {name: 'Agudelo Test', address: 'ae900k86d': users_voters: [] },
-    {name: 'Giraldo Dev Test', address: 'cja2abje6': users_voters: [] },
-    {name: 'Lean Test', address: 'ja1ml1hfc': users_voters: [] },
+    {name: 'Andres Giraldo', address: '5biilg2kd', users_voters: [], votes: 0 },
+    {name: 'Felipe Prueba', address: 'l21c41i72', users_voters: [], votes: 0 },
+    {name: 'Agudelo Test', address: 'ae900k86d', users_voters: [], votes: 0 },
+    {name: 'Giraldo Dev Test', address: 'cja2abje6', users_voters: [], votes: 0 },
+    {name: 'Lean Test', address: 'ja1ml1hfc', users_voters: [], votes: 0 },
 ]
 
 export default users
