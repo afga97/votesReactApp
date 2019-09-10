b88e9ec0228d573c5bd1e7e7a41c4e7d92c29d5c
Andres <afga199714@gmail.com>
2019-09-09 23:19:00 -0500

    List users

--- src/UsersData.js
+++ src/UsersData.js
@@ -1,9 +1,9 @@
 const users = [
-    {user: 'Andres Giraldo', address: '5biilg2kd', users_voters: [], votes: 0 },
-    {user: 'Felipe Prueba', address: 'l21c41i72', users_voters: [], votes: 0 },
-    {user: 'Agudelo Test', address: 'ae900k86d', users_voters: [], votes: 0 },
-    {user: 'Giraldo Dev Test', address: 'cja2abje6', users_voters: [], votes: 0 },
-    {user: 'Lean Test', address: 'ja1ml1hfc', users_voters: [], votes: 0 },
+    {name: 'Andres Giraldo', address: '5biilg2kd', amCandidate: true, users_voters: [], votes: 0 },
+    {name: 'Felipe Prueba', address: 'l21c41i72', amCandidate: true, users_voters: [], votes: 0 },
+    {name: 'Agudelo Test', address: 'ae900k86d', amCandidate: false, users_voters: [], votes: 0 },
+    {name: 'Giraldo Dev Test', address: 'cja2abje6', amCandidate: false, users_voters: [], votes: 0 },
+    {name: 'Lean Test', address: 'ja1ml1hfc', amCandidate: true, users_voters: [], votes: 0 },
 ]
 
 export default users
--- src/components/user/ListUser.js
+++ src/components/user/ListUser.js
@@ -1,36 +1,45 @@
-import React, { Component } from 'react';
-import { Card, CardHeader, CardBody, Table } from 'reactstrap';
+import React from 'react';
+import { Card, CardHeader, CardBody, Table, Badge } from 'reactstrap';
 
-class ListUser extends Component  {
-
-    constructor(props) {
-        super(props)
+const RowsUser = (props) => {
+    const getBadge = (status) => {
+        return status ? 'success' : 'danger'
     }
+    
+    return !props.users ? <tr><td colSpan="4" className="text-center">No results found</td></tr> :
+        props.users.map((e, i) => (
+            <tr key={i}>
+                <td>{e.name}</td>
+                <td>{e.address}</td>
+                <td>{e.amCandidate ? 'true': 'false'}</td>
+                <td><Badge color={getBadge(e.amCandidate)}>New</Badge></td>
+            </tr>
+        ))
+}
 
-    render(){
-        return (
-            <Card>
-                <CardHeader>
-                    <i className="fa fa-align-justify"></i> Registered Voters
-                </CardHeader>
-                <CardBody>
-                    <Table responsive>
-                        <thead>
-                            <tr>
-                                <th>Fullname</th>
-                                <th>Address</th>
-                                <th>Candidate?</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            
-                        </tbody>
-                    </Table>
-                </CardBody>
-            </Card>
-        );
-    }
+const ListUser = (props) => {
+    return (
+        <Card>
+            <CardHeader>
+                <i className="fa fa-align-justify"></i> Registered Voters
+            </CardHeader>
+            <CardBody>
+                <Table responsive>
+                    <thead>
+                        <tr>
+                            <th>Fullname</th>
+                            <th>Address</th>
+                            <th>Candidate?</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <RowsUser users={props.users} />
+                    </tbody>
+                </Table>
+            </CardBody>
+        </Card>
+    );
 }
 
 export default ListUser
